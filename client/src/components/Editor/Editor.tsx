aef974a409f34d6419544093f515336eafb33852
1F10220146 <s1f102201469@iniad.org>
2023-08-11 14:20:51 +0900

    fix: レビューされた部分を修正

--- client/src/components/Editor/Editor.tsx
+++ client/src/components/Editor/Editor.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react';
+import Editor from '@monaco-editor/react';
 
 const rTabs = (str: string) => str.trim().replace(/^ {4}/gm, '');
-
-import Editor from '@monaco-editor/react';
 
 const examples = {
   userinfo: rTabs(`
@@ -16,18 +14,14 @@ const examples = {
 };
 
 function MonacoEditor() {
-  const [language, setLanguage] = useState('javascript');
-
   return (
-    <>
-      <Editor
-        height="100%"
-        theme={'vs-dark'}
-        language={language}
-        value={examples.userinfo}
-        options={{ readOnly: true }} // ここで読み取り専用に設定
-      />
-    </>
+    <Editor
+      height="100%"
+      theme={'vs-dark'}
+      language={'javascript'}
+      value={examples.userinfo}
+      options={{ readOnly: true }} // ここで読み取り専用に設定
+    />
   );
 }
 
